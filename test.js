0f2ee5d4932a690e8791c960751e18daac17ce52
kkong7 <kkongk@naver.com>
2020-03-23 05:55:50 +0900

    v202003231755

--- test.js
+++ test.js
@@ -1,6 +1,2 @@
-var http=require('http');
-
-http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+var date=new Date();
+console.log(date.getHours()+':'+date.getMinutes());

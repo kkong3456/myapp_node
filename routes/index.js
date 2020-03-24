294c412bdf69d1a62bf82ebf2049660145b3415f
kkong7 <kkongk@naver.com>
2020-03-24 12:04:19 +0900

    v202003241204

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '가칭' });
+  res.render('index', { title: 'CSCF#1' });
+  next();
+});
+
+router.get('/cscf1',function(req,res,next){
+  res.render('cscf1',{title:'CSCF#1'});
+  next();
+});
+
+router.get('/cscf2',function(req,res,next){
+  res.render('cscf2',{title:'CSCF#2'});
+  next();
+});
+
+router.get('/cscf3',function(req,res,next){
+  res.render('cscf3',{title:'CSCF#3'});
+  next();
+});
+
+router.get('/cscf4',function(req,res,next){
+  res.render('cscf4',{title:'CSCF#4'});
+  next();
+});
+
+router.get('/cscf5',function(req,res,next){
+  res.render('cscf5',{title:'CSCF#5'});
+  next();
+});
+
+router.get('/vcscf1',function(req,res,next){
+  res.render('vcscf1',{title:'vCSCF#1'});
+  next();
+});
+
+router.get('/vcscf2',function(req,res,next){
+  res.render('vcscf2',{title:'vCSCF#2'});
+  next();
 });
 
 module.exports = router;
